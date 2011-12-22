d26856a2d34f244b09449437b0a2fb272a4dd7f9
Paul Graphov <graphov@gmail.com>
2011-12-22 17:07:13 +0400

    initial

---
+++ xmonad.hs
@@ -0,0 +1,97 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Config.Xfce
+
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+main = xmonad myXfceConfig
+
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
+myXfceConfig = xfceConfig
+    { terminal           = "urxvt"
+    , keys               = myKeys
+    , normalBorderColor  = "#222222"
+    , focusedBorderColor = "#444444"
+    , handleEventHook    = ewmhDesktopsEventHook
+    , startupHook        = ewmhDesktopsStartup               
+    , workspaces         = myWorkspaces
+    , manageHook         = myManageHook <+> manageHook xfceConfig
+    }
+
+myManageHook = composeAll [
+                           isFullscreen --> doFloat
+                          ]
+
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+ 
+    -- launch a terminal
+    [ ((mod4Mask, xK_Return), spawn $ XMonad.terminal conf)
+ 
+    -- launch "run application"
+    , ((mod4Mask, xK_r     ), spawn "gmrun")
+ 
+    -- launch emacs
+    , ((mod4Mask, xK_e     ), spawn "emacs")
+
+    -- launch browser
+    , ((mod4Mask, xK_f     ), spawn "chromium-browser")
+    
+    -- launch mail
+    , ((mod4Mask, xK_m     ), spawn "thunderbird")
+
+    -- launch banshee
+    , ((mod4Mask, xK_b     ), spawn "gmusicbrowser")
+
+    -- launch pidgin
+    , ((mod4Mask, xK_g     ), spawn "pidgin")
+ 
+    -- close focused window 
+    , ((mod4Mask, xK_c     ), kill)
+ 
+    -- Rotate through the available layout algorithms
+    , ((mod4Mask,               xK_space ), sendMessage NextLayout)
+ 
+    -- Resize viewed windows to the correct size
+    , ((mod4Mask,               xK_n     ), refresh)
+ 
+    -- Move focus to the next window
+    , ((mod4Mask,                xK_Tab   ), windows W.focusDown)
+ 
+    -- Swap the focused window and the master window
+    , ((mod4Mask,               xK_BackSpace), windows W.swapMaster)
+ 
+    -- Shrink the master area
+    , ((mod4Mask,               xK_Left     ), sendMessage Shrink)
+ 
+    -- Expand the master area
+    , ((mod4Mask,               xK_Right     ), sendMessage Expand)
+ 
+    -- Push window back into tiling
+    , ((mod4Mask,               xK_Home     ), withFocused $ windows . W.sink)
+ 
+    -- Increment the number of windows in the master area
+    , ((mod4Mask              , xK_Up ), sendMessage (IncMasterN 1))
+ 
+    -- Deincrement the number of windows in the master area
+    , ((mod4Mask              , xK_Down), sendMessage (IncMasterN (-1)))
+ 
+    -- toggle the status bar gap
+    -- TODO, update this binding with avoidStruts , ((mod4Mask              , xK_b     ),
+    ]
+    ++
+ 
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. mod4Mask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
